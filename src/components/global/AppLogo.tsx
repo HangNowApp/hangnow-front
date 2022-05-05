c36173478e3a41a668f8918931182f0096d4c63a
Alexandre Javet <alexandre.javet@hotmail.com>
2022-05-05 10:17:09 +0200

    Update AppLogo.tsx

--- src/components/global/AppLogo.tsx
+++ src/components/global/AppLogo.tsx
@@ -2,11 +2,11 @@ import { Box, Typography } from '@mui/material';
 
 export default function AppLogo() {
   return (
-      <Typography variant="h4" fontWeight="bold" fontSize={24}>
-        Hang
-        <Box color="primary.main" component="span">
-          Now!
-        </Box>
-      </Typography>
+    <Typography variant="h4" fontWeight="bold" fontSize={24}>
+      Hang
+      <Box color="primary.main" component="span">
+        Now!
+      </Box>
+    </Typography>
   );
 }
