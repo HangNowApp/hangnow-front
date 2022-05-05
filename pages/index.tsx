ba2b3c0127af45c49ed633dd854fad729815aba9
villou <dkulic01@gmail.com>
2022-05-05 11:09:28 +0200

    add index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
+import { Box } from '@mui/material';
 import type { NextPage } from 'next';
-import MeetEventCard from '~/components/event/meet-event-card/MeetEventCard';
+import { MeetEventCard } from '~/components/event/meet-event-card';
 
 const Home: NextPage = () => (
   <div>
@@ -7,6 +8,7 @@ const Home: NextPage = () => (
       title="Title of event"
       time="3m ago"
       imageUrl="https://www.wbcsd.org/var/site/storage/images/media/images/finance_img/25992-1-eng-GB/finance_img_i1140.jpg"
+      tag={['Finances', 'Chill']}
     ></MeetEventCard>
   </div>
 );
