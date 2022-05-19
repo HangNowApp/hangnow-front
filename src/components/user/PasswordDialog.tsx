a857e73960cc3885805b658b541c7e4984d8c621
villou <dkulic01@gmail.com>
2022-05-19 08:27:48 +0200

    password update

--- src/components/user/PasswordDialog.tsx
+++ src/components/user/PasswordDialog.tsx
@@ -6,32 +6,61 @@ import {
   DialogActions,
   Box,
 } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
+import { clientJson } from '~/client/client';
+import { AuthResponse } from '~/client/types/Auth';
 
 type PasswordDialogProps = {
   open: boolean;
   onClose: () => void;
-  onSave: () => void;
 };
 
-export function PasswordDialog({ open, onClose, onSave }: PasswordDialogProps) {
+export function PasswordDialog({ open, onClose }: PasswordDialogProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
+
+  const updatePassword: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event?.preventDefault();
+    const oldpassword = event.currentTarget.oldpassword.value;
+    const newpassword = event.currentTarget.newpassword.value;
+
+    setIsLoading(true);
+    clientJson<AuthResponse>('auth/change_password', {
+      method: 'PATCH',
+      data: {
+        oldpassword,
+        newpassword,
+      },
+    })
+      .then(() => {
+        setIsLoading(false);
+        onClose();
+      })
+      .catch((err) => {
+        console.error('err', err);
+        setIsLoading(false);
+      });
+  };
+
   return (
     <Dialog open={open} onClose={() => onClose()}>
-      <DialogContent>
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-          <TextField type="password" label="Old Password" />
-          <TextField type="password" label="New Password" />
-        </Box>
-      </DialogContent>
+      <form onSubmit={updatePassword}>
+        <DialogContent>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            <TextField id="oldpassword" type="password" label="Old Password" />
+            <TextField id="newpassword" type="password" label="New Password" />
+          </Box>
+        </DialogContent>
 
-      <DialogActions>
-        <Button size="small" onClick={() => onClose()}>
-          Cancel
-        </Button>
-        <Button size="small" onClick={() => onSave()}>
-          Save
-        </Button>
-      </DialogActions>
+        <DialogActions>
+          <Button size="small" onClick={() => onClose()}>
+            Cancel
+          </Button>
+          <Button type="submit" size="small">
+            Save
+          </Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 }
--- src/components/user/UserProfile.tsx
+++ src/components/user/UserProfile.tsx
@@ -17,6 +17,7 @@ export function UserProfile() {
   const user = authContext.user;
   const [isEditing, setEditing] = useState(false);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
 
   const textFieldCommonProps: TextFieldProps = {
     disabled: !isEditing,
@@ -28,18 +29,6 @@ export function UserProfile() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
-  const updatePassword = () => {
-    setIsLoading(true);
-    clientJson<AuthResponse>('auth/change_password')
-      .then((res) => {
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        console.error('err', err);
-        setIsLoading(false);
-      });
-  };
 
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
     event?.preventDefault();
@@ -64,67 +53,65 @@ export function UserProfile() {
   };
 
   return (
-    <Box
-      component={'form'}
-      onSubmit={handleSubmit}
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 2,
-        margin: 'auto',
-      }}
-    >
+    <>
       <Box
+        component={'form'}
+        onSubmit={handleSubmit}
         sx={{
           display: 'flex',
           flexDirection: 'column',
           gap: 2,
-          alignItems: 'center',
+          margin: 'auto',
         }}
       >
-        <Typography variant="h4">{user?.userName}'s profile page</Typography>
-        <Avatar
-          alt="avatar"
-          src={
-            user?.avatarUrl ??
-            'https://i.pinimg.com/474x/2f/ec/a4/2feca4c9330929232091f910dbff7f87.jpg'
-          }
-          sx={{ width: 80, height: 80 }}
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+            alignItems: 'center',
+          }}
+        >
+          <Typography variant="h4">{user?.userName}'s profile page</Typography>
+          <Avatar
+            alt="avatar"
+            src={
+              user?.avatarUrl ??
+              'https://i.pinimg.com/474x/2f/ec/a4/2feca4c9330929232091f910dbff7f87.jpg'
+            }
+            sx={{ width: 80, height: 80 }}
+          />
+        </Box>
+
+        <TextField
+          {...textFieldCommonProps}
+          id="username"
+          label="Username"
+          defaultValue={user?.userName}
         />
+        <TextField
+          {...textFieldCommonProps}
+          id="email"
+          type="email"
+          label="Email"
+          defaultValue={user?.email}
+        />
+        <TextField
+          {...textFieldCommonProps}
+          id="avatarUrl"
+          label="AvatarURL"
+          defaultValue={user?.avatarUrl ?? ''}
+        />
+
+        <Button size="small" onClick={handleOpen}>
+          Change password
+        </Button>
+
+        <Button size="small" type="submit" variant="contained">
+          {isEditing ? 'Save' : 'Edit'}
+        </Button>
       </Box>
-
-      <TextField
-        {...textFieldCommonProps}
-        id="username"
-        label="Username"
-        defaultValue={user?.userName}
-      />
-      <TextField
-        {...textFieldCommonProps}
-        id="email"
-        type="email"
-        label="Email"
-        defaultValue={user?.email}
-      />
-      <TextField
-        {...textFieldCommonProps}
-        id="avatarUrl"
-        label="AvatarURL"
-        defaultValue={user?.avatarUrl ?? ''}
-      />
-
-      <Button size="small" onClick={handleOpen}>
-        Change password
-      </Button>
-      <PasswordDialog
-        open={open}
-        onClose={handleClose}
-        onSave={updatePassword}
-      />
-
-      <Button size="small" type="submit" variant="contained">
-        {isEditing ? 'Save' : 'Edit'}
-      </Button>
-    </Box>
+      <PasswordDialog open={open} onClose={handleClose} />
+    </>
   );
 }
